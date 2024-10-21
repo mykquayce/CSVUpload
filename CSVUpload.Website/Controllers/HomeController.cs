b9e81d3bfbc43f703c25222fc015885440db0e3a
bob h <mail@bob.codes>
2024-10-21 16:34:57 +0100

    initial commit

---
+++ CSVUpload.Website/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using CSVUpload.Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CSVUpload.Website.Controllers;
+public class HomeController : Controller
+{
+	private readonly ILogger<HomeController> _logger;
+
+	public HomeController(ILogger<HomeController> logger)
+	{
+		_logger = logger;
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
+}
