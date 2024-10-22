f10c377afcd6bab7050fac04b19b8df8b583202f
bob h <mail@bob.codes>
2024-10-22 10:11:08 +0100

    copy 'n paste file upload from tutorial
    https://code-maze.com/file-upload-aspnetcore-mvc/

--- CSVUpload.Website/Controllers/HomeController.cs
+++ CSVUpload.Website/Controllers/HomeController.cs
@@ -27,4 +27,32 @@ public class HomeController : Controller
 	{
 		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 	}
+
+	[HttpPost]
+	public async Task<IActionResult> FileUpload(IReadOnlyCollection<IFormFile> files)
+	{
+		var tasks = files.Where(f => f?.Length > 0).Select(f => UploadAsync(f));
+		var tempFiles = await Task.WhenAll(tasks);
+
+		try
+		{
+			return Ok(new { count = files.Count, size = tempFiles.Sum(fi => fi.Length), filePaths = tempFiles.Select(fi => fi.FullName), });
+		}
+		finally
+		{
+			base.Response.OnCompleted(async () =>
+			{
+			});
+		}
+	}
+
+	private static async Task<FileInfo> UploadAsync(IFormFile formFile, CancellationToken cancellationToken = default)
+	{
+		ArgumentNullException.ThrowIfNull(formFile);
+		ArgumentOutOfRangeException.ThrowIfZero(formFile.Length);
+		var tempFile = new FileInfo(fileName: Path.GetTempFileName());
+		await using var stream = tempFile.OpenWrite();
+		await formFile.CopyToAsync(stream, cancellationToken);
+		return tempFile;
+	}
 }
